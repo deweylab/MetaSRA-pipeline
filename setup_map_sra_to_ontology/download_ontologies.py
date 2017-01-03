e8909d47626d2fdcee6944ebb2944eef02026291
Matthew Bernstein <berniematt18@gmail.com>
2017-01-03 12:00:46 -0600

    Cleaned download ontologies code to make configuration easier

--- setup_map_sra_to_ontology/download_ontologies.py
+++ setup_map_sra_to_ontology/download_ontologies.py
@@ -7,28 +7,18 @@ from os.path import join
 
 def main():
     parser = OptionParser()
-    #parser.add_option("-a", "--a_descrip", action="store_true", help="This is a flat")
-    #parser.add_option("-b", "--b_descrip", help="This is an argument")
     (options, args) = parser.parse_args()
 
     obo_rel_loc = "../map_sra_to_ontology/obo"
-    name_to_prefix = {
-        "uberon":"UBERON",
-        "cl": "CL",
-        "doid":"DOID",
-        "cellosaurus":"CVCL",
-        "uo":"UO",
-        "chebi":"CHEBI",
-        "efo":"EFO"}
-
+    
     prefix_to_filename = {}
     date_str = datetime.datetime.now().strftime("%y-%m-%d")
     with open("ontology_name_to_url.json", "r") as f:
-        for ont_name, url in json.load(f).iteritems():
-            obo_f_name = join(obo_rel_loc, "%s.%s.obo" % (ont_name, date_str))
+        for ont_prefix, url in json.load(f).iteritems():
+            obo_f_name = join(obo_rel_loc, "%s.%s.obo" % (ont_prefix, date_str))
             output_f = open(obo_f_name, "w")
             subprocess.call(["curl", url], stdout=output_f)   
-            prefix_to_filename[name_to_prefix[ont_name]] = "%s.%s.obo" % (ont_name, date_str)
+            prefix_to_filename[ont_prefix] = "%s.%s.obo" % (ont_prefix, date_str)
 
     with open("../map_sra_to_ontology/ont_prefix_to_filename.json", "w") as f:
         f.write(json.dumps(prefix_to_filename, indent=4, separators=(',', ': '))) 
